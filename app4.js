2c4973529b9ba4f611b1fbcb2c118e3616529fab
lifeliine <matias.cespedes@outlook.com>
2021-11-06 21:08:39 -0300

    cambio doble 53

---
+++ app4.js
@@ -0,0 +1,17 @@
+setTimeout( () => {
+    console.log("Primer timeout");
+}, 5000);
+
+setTimeout( () => {
+    console.log("Primer timeout");
+}, 3000);
+
+setTimeout( () => {
+    console.log("Primer timeout");
+}, 5000);
+
+setTimeout( () => {
+    console.log("Primer timeout");
+}, 3000);
+
+
